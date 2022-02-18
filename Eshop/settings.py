6579de3cb6d255aab5a8d6a18dceae6b2f00c226
samuelkaranja <samuelkaranja007@gmail.com>
2022-02-18 23:21:26 +0300

    Initial Update

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yp#bw6s^u4ffrf2qc-^66%-1&2gxvvaj*e^&8-5gplyywb6%*x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sams-e-shop.herokuapp.com']
+ALLOWED_HOSTS = ['sams-e-shop.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -122,7 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
